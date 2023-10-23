c0d9569813f3362eadcda9df8e758b1d7b10cba9
Tokal <SeanSMaseola@gmail.com>
2023-10-23 08:53:29 +0200

    Add project files.

---
+++ TestingForPK/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TestingForPK
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(56, 303);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Gen";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(350, 307);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "Check";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(620, 308);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 2;
+            button3.Text = "Change";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(42, 17);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(274, 262);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(375, 21);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(413, 258);
+            panel2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Panel panel1;
+        private Panel panel2;
+    }
+}
---
+++ TestingForPK/Form1.cs
@@ -0,0 +1,122 @@
+namespace TestingForPK
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> Group14 = new Dictionary<string, string>();
+        Dictionary<string, string> AnswerDic = new Dictionary<string, string>();
+        Random ran = new Random();
+        int TempNumScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Group14.Add("PK", "Sound of Depression");
+            Group14.Add("Simphiwe", "Wizard of Hearts");
+            Group14.Add("Siya", "Nightmare of HR");
+            Group14.Add("Nhlanhla", "Myth of Standards");
+            Group14.Add("Samir", "Defier of Odds");
+            Group14.Add("Sean", "Beast of Gevaudan");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> callNumbers = Group14.Keys.ToList();
+            List<string> calDescription = Group14.Values.ToList();
+
+            int labelY1 = 0; // Initialize the Y coordinate for the first label
+            int labelY2 = 0; // Initialize the Y coordinate for the first label
+
+            foreach (string x in callNumbers)
+            {
+                Label callLabel = new Label
+                {
+                    Text = x,
+                    Cursor = Cursors.Hand,
+                    Font = new System.Drawing.Font("Arial", 12),
+                    Width = 100,
+                    Height = 30,
+                    Margin = new Padding(5),
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+
+                // Set the location (X, Y) for the label
+                callLabel.Location = new Point(0, labelY1);
+
+                callLabel.Click += CallLabel_Click;
+                panel1.Controls.Add(callLabel);
+
+                // Adjust the Y coordinate for the next label
+                labelY1 += callLabel.Height + 10; // You can adjust the spacing as needed
+            }
+            foreach (string x in calDescription.OrderBy(r => ran.Next()))
+            {
+
+
+                Label definitionTextBlock = new Label
+                {
+                    Text = x,
+                    Cursor = Cursors.Hand,
+                    Font = new System.Drawing.Font("Arial", 12),
+                    Width = 300,
+                    Height = 30,
+                    Margin = new Padding(5),
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+
+                definitionTextBlock.Location = new Point(0, labelY2);
+
+                panel1.Controls.Add(definitionTextBlock);
+
+                // Adjust the Y coordinate for the next label
+                labelY2 += definitionTextBlock.Height + 10;
+                definitionTextBlock.Click += Definition_MouseLeftButtonClick;
+
+                panel2.Controls.Add(definitionTextBlock);
+
+            }
+        }
+
+        private Label callNumberTextBlock = null;
+        private void CallLabel_Click(object sender, EventArgs e)
+        {
+            callNumberTextBlock = (Label)sender;
+            callNumberTextBlock.BackColor = Color.Yellow;
+        }
+
+        private void Definition_MouseLeftButtonClick(object sender, EventArgs e)
+        {
+            if (callNumberTextBlock != null)
+            {
+                Label descriptTextBlock = (Label)sender;
+                AnswerDic.Add(callNumberTextBlock.Text, descriptTextBlock.Text);
+                callNumberTextBlock.BackColor = Color.Gray;
+                callNumberTextBlock.IsAccessible = false;
+                descriptTextBlock.BackColor = Color.Gray;
+                descriptTextBlock.IsAccessible = false;
+
+                callNumberTextBlock = null;
+            }
+            else
+            {
+                MessageBox.Show("Select Call Number First");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (KeyValuePair<string, string> x in Group14)
+            {
+                if (AnswerDic.TryGetValue(x.Key, out string area))
+                {
+                    if (x.Value.Equals(area))
+                    {
+                        TempNumScore++;
+                    }
+                }
+            }
+
+            MessageBox.Show("Your Score is: " + TempNumScore);
+        }
+    }
+}
